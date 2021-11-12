caaaea53e9f88cef4fe0748670dabc537d69dd36
Ben Welch <benwelch@bens-air-7.wlan.dur.ac.uk>
2021-11-12 19:23:46 +0000

    test 2 (sorry josh's bank account)

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-  }
+function resizeIframe(bool) {
+    if (bool == true) document.getElementById("navbarIframe").style.height = "344px";
+    else document.getElementById("navbarIframe").style.height = "61px";
+}
