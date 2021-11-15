0671338e27a9b7c724eacdfe3f27e41cda56dfe3
Ben Welch <benwelch@bens-air-7.wlan.dur.ac.uk>
2021-11-15 17:33:58 +0000

    updates

--- main.js
+++ main.js
@@ -2,3 +2,38 @@ function resizeIframe(bool) {
     if (bool == true) document.getElementById("navbarIframe").style.height = "344px";
     else document.getElementById("navbarIframe").style.height = "61px";
 }
+
+function resizeIndex() {
+    
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    if(w/h < 1.5 ){
+        h2 = w/1.5;
+        document.getElementById("indexFrame").style.height = h2+"px";
+    }
+    else document.getElementById("indexFrame").style.height = h+"px";
+    if(w/h < 1.25) {
+        document.getElementById("divide").style.display = "none";
+
+        document.getElementById("headerTXT").style.display = "inline-block";
+        document.getElementById("headerIMG").style.display = "inline-block";
+        document.getElementById("headerTXT").style.width = "80%";
+        document.getElementById("headerIMG").style.width = "15%";
+
+        document.getElementById("arrow").style.display = "none";
+    }
+    else {
+        document.getElementById("divide").style.display = "initial";
+
+        document.getElementById("headerTXT").style.display = "initial";
+        document.getElementById("headerIMG").style.display = "initial";
+        document.getElementById("headerTXT").style.width = "";
+        document.getElementById("headerIMG").style.width = "175px";
+
+        document.getElementById("arrow").style.display = "initial";
+    }
+}
+
+function scrollTo(element) {
+    document.getElementById(element).scrollIntoView();
+}
