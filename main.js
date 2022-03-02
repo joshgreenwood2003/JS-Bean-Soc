571a7eb2b97606249eb4536f837dbaeb05ef2260
Ben Welch <benwelch@Bens-MacBook-Air-10.local>
2022-03-02 14:08:52 +0000

    minor fixes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function resizeIframe(bool) {
-    if (bool == true) document.getElementById("navbarIframe").style.height = "344px";
+    if (bool == true) document.getElementById("navbarIframe").style.height = "381px";
     else document.getElementById("navbarIframe").style.height = "61px";
 }
 
@@ -36,31 +36,4 @@ function resizeIndex() {
 
 function scrollTo(element) {
     document.getElementById(element).scrollIntoView();
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
